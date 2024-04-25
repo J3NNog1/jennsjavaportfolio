ef0ba4588cec0633f6b0b7ff227f11c85c3413a1
jennifer j <jenniferjuarezjj@gmail.com>
2024-04-25 14:40:37 -0400

    create project with dependencies using spring initilizer

---
+++ src/main/java/com/jennsportfolio/Spring__Portfolio/SpringPortfolioApplication.java
@@ -0,0 +1,13 @@
+package com.jennsportfolio.Spring__Portfolio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringPortfolioApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringPortfolioApplication.class, args);
+	}
+
+}
